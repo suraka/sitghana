a399014b8c0f1227cd9788dbe2b2aaf2aeb13134
suraka <surakaobliya@gmail.com>
2018-07-08 01:56:50 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyD-xc1rVzr1udbJoZDWm3QHYRpKbDS9zgc',
-    authDomain: 'sitghana-c73c6.firebaseapp.com',
-    databaseURL: 'https://sitghana-c73c6.firebaseio.com',
-    projectId: 'sitghana-c73c6',
-    storageBucket: 'sitghana-c73c6.appspot.com',
-    messagingSenderId: '2348774764'
+    
   }
 };
 
